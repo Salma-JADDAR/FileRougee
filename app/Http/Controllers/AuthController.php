deb46d396bad2830ed525a0820c031cc85c449e5
Salma GTU <salmajaddar7@gmail.com>
2026-03-31 10:51:37 +0200

    Implémentation de la logique de traitement du login utilisateur dans le controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,14 +9,12 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller{
     
-    public function showLogin()
-    {
+    public function showLogin(){
         return view('auth.login');
     }
     
-    // Traiter login
-    public function login(Request $request)
-    {
+    
+    public function login(Request $request){
         $credentials = $request->validate([
             'email' => 'required|email',
             'password' => 'required'
